685c8586b7015e324f9de327057dc5c75e34a065
Iraton <notari.filippo@outlook.it>
2023-11-04 21:38:16 +0100

    Added homepage grid and slideshow (test)

--- Script/script.js
+++ Script/script.js
@@ -39,3 +39,20 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+//slideshow
+let slideIndex = 0;
+
+function plusSlides(n) {
+  slideIndex += n
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (slideIndex > slides.length) {
+    slideIndex = 1 
+  }
+  if (slideIndex < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
